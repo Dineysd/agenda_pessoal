d87d3b34bb27a16efe51c446043b0ed137097081
Dineysd <38842411+Dineysd@users.noreply.github.com>
2020-01-09 21:20:28 -0300

    configurando categoria

---
+++ agenda_pessoal/src/main/java/com/projeto/agenda_pessoal/AgendaPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.agenda_pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaPessoalApplication.class, args);
+	}
+
+}
---
+++ agenda_pessoal/src/test/java/com/projeto/agenda_pessoal/AgendaPessoalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projeto.agenda_pessoal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgendaPessoalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
