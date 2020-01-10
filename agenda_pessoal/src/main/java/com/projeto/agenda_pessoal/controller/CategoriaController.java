c0c458111ba76e1af394ac2b00a0a55cc7aeb2f4
Dineysd <38842411+Dineysd@users.noreply.github.com>
2020-01-09 23:05:51 -0300

    criando categoria controller

---
+++ agenda_pessoal/src/main/java/com/projeto/agenda_pessoal/controller/CategoriaController.java
@@ -0,0 +1,55 @@
+package com.projeto.agenda_pessoal.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projeto.agenda_pessoal.model.Categoria;
+import com.projeto.agenda_pessoal.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public List<Categoria> listar(){
+		return repository.findAll();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = repository.save(categoria);
+		// retorna oa uri da categoria que foi salva
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+		.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPorCodigo(@PathVariable Long codigo) {
+		return repository.findById(codigo)
+				.map(categoria -> ResponseEntity.ok(categoria))
+			      .orElse(ResponseEntity.notFound().build());
+	}
+}
